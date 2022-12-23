e74366e75174b8ca17ad495d7286bb8fe9a15aae
Vaibhav Waghule <107881782+WAGHULE2001@users.noreply.github.com>
2022-12-23 16:08:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/listing',(req,res) => {
     })
 })
 
-return.post('/register',(req,res) => {
+render.post('/register',(req,res) => {
     var hashedpassword = bcrypt.hashSync(req.body.password,8);
     User.create({
         name:req.body.name,
